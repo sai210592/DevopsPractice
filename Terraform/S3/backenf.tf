c23500be9bfec79975eb75b9ff7d11a6249a8641
sai210592 <108013075+sai210592@users.noreply.github.com>
2025-02-10 02:43:18 +0530

    Update backenf.tf

--- Terraform/S3/backenf.tf
+++ Terraform/S3/backenf.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "sai210592"
+    key            = "prod/ec2.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "state-locking"
 
+  }
+}
